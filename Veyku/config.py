1331eb48d2d8b5893736ae5ea7f1d7acbae82433
Automatum2 <jayajeje63@gmail.com>
2026-04-27 14:05:38 +0800

    Delete Veyku directory

--- Veyku/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'smk1-denpasar-survei-secret-key-2026'
-    
-    # Konfigurasi MySQL (XAMPP)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:@localhost/majormind_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- Veyku/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from models.user import User
-from models.respondent import Respondent
-from models.survey_response import SurveyResponse
