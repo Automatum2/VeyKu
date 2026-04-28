cf3322b7dc06b6ed1fc0b4e21fc2bc477f0c52a0
Automatum2 <jayajeje63@gmail.com>
2026-04-29 00:57:47 +0800

    cuman ubah dikit

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+# Load file .env jika ada
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'smk1-denpasar-survei-secret-key-2026'
     
-    # Konfigurasi MySQL (XAMPP)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:@localhost/majormind_db'
+    # Deteksi apakah berjalan di server (misal: PythonAnywhere atau Render)
+    # Jika ada DATABASE_URL di environment variable, gunakan itu.
+    # Jika tidak, gunakan MySQL lokal (XAMPP).
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'mysql+pymysql://root:@localhost/majormind_db'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
